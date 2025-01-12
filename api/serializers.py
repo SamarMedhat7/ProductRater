3cf0b4d310c0d6815c007cf264655f0d8ce9a672
SamarMedhat7 <samarmedhat573@gmail.com>
2025-01-12 02:07:18 +0200

    feat : token-user

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import Product, Rating
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 
-
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
--- api/views.py
+++ api/views.py
@@ -1,17 +1,43 @@
 from rest_framework import viewsets,request,status
 from .models import Product, Rating
-from .serializers import ProductSerializer, RatingSerializer
+from .serializers import ProductSerializer, RatingSerializer, UserSerializer
 
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from django.contrib.auth.models import User
 
+from rest_framework.authentication import TokenAuthentication
+
+from rest_framework.permissions import  AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+
+from rest_framework.authtoken.models import Token
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # #authentication_classes = (TokenAuthentication, )
+    permission_classes = (AllowAny,)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        token, created = Token.objects.get_or_create(user=serializer.instance)
+        return Response({
+                'token': token.key, 
+                }, 
+            status=status.HTTP_201_CREATED)
+    
+    def list(self, request, *args, **kwargs):
+        response = {'message': 'You cant create rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated,)
 
     
     @action(detail=True, methods=['post'])
@@ -22,8 +48,7 @@ class ProductViewSet(viewsets.ModelViewSet):
             '''
             product = Product.objects.get(id=pk)
             stars = request.data['stars']
-            username = request.data['username']
-            user = User.objects.get(username=username)
+            user = request.user
 
             try:
                 # update
@@ -35,7 +60,7 @@ class ProductViewSet(viewsets.ModelViewSet):
                     'message': 'Product Rate Updated',
                     'result': serializer.data
                 }
-                return Response(json , status=status.HTTP_202_ACCEPTED)
+                return Response(json , status=status.HTTP_400_BAD_REQUEST)
 
             except:
                 # create if the rate not exist 
@@ -45,7 +70,7 @@ class ProductViewSet(viewsets.ModelViewSet):
                     'message': 'Meal Rate Created',
                     'result': serializer.data
                 }
-                return Response(json , status=status.HTTP_201_CREATED)
+                return Response(json , status=status.HTTP_200_OK)
 
         else:
             json = {
@@ -57,3 +82,20 @@ class ProductViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated)
+    
+    def update(self, request, *args, **kwargs):
+        response = {
+            'message': 'Invalid way to create or update '
+            }
+
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    
+    def create(self, request, *args, **kwargs):
+        response = {
+            'message': 'Invalid way to create or update '
+            }
+
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
